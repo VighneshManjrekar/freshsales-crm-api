cfc0bd7c6f784ded91131943cd158549bfb413a1
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2022-08-09 16:16:58 +0530

    Security headers

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-require("dotenv").config({path:"./configs/.env"})
+require("dotenv").config({ path: "./configs/.env" });
 
 const express = require("express");
+
+// Security pkgs
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 // import router
 const crmRoutes = require("./routes/crm.route");
 
@@ -9,6 +17,22 @@ const PORT = process.env.PORT || 7000;
 
 // body-parser middleware
 app.use(express.json());
+
+// Security headers
+app.use(helmet());
+// Prevent XSS
+app.use(xss());
+// Limit req rate
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 100, // Each ip maximum 100 requests per 15 minutes
+});
+app.use(limiter);
+// Protect against HTTP Parameter Pollution attacks
+app.use(hpp());
+// Enable CORS
+app.use(cors());
+
 // mount router
 app.use(crmRoutes);
 
